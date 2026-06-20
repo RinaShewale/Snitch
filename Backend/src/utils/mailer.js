0ef229e4910fe492d9e57bd7be8f11fc39f9b2c5
rinashewale111 <rinashewale6@gmail.com>
2026-06-21 00:22:12 +0530

    fix: changing brevo email

--- Backend/src/utils/mailer.js
+++ Backend/src/utils/mailer.js
@@ -1,21 +1,14 @@
 import nodemailer from "nodemailer";
 import { config } from "../config/config.js";
 
-console.log("EMAIL:", config.BREVO_EMAIL);
-console.log("KEY EXISTS:", !!config.BREVO_SMTP_KEY);
-
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: config.BREVO_EMAIL,
     pass: config.BREVO_SMTP_KEY,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 transporter.verify((error) => {
