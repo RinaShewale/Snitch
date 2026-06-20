17035c19e851140f169312daf4286197c9c40df5
rinashewale111 <rinashewale6@gmail.com>
2026-06-20 23:36:40 +0530

    fix: mailer fixing

--- Backend/src/utils/mailer.js
+++ Backend/src/utils/mailer.js
@@ -2,15 +2,14 @@ import nodemailer from "nodemailer";
 import { config } from "../config/config.js";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: config.EMAIL_USER,
     pass: config.EMAIL_PASS,
   },
-
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 transporter.verify((error) => {
