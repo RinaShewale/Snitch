92b303418eecbff2be80ed9e4f44e7511cf86cee
rinashewale111 <rinashewale6@gmail.com>
2026-06-21 16:30:04 +0530

    deplaoymenet

--- Backend/src/routes/review.route.js
+++ Backend/src/routes/review.route.js
@@ -1,18 +1,23 @@
 import express from "express";
-import { createReview, getProductReviews } from "../controller/review.controller.js";
+import {
+  createReview,
+  getProductReviews,
+} from "../controller/review.controller.js";
+
 import { protect } from "../middlewares/auth.middleware.js";
 import { upload } from "../services/multer.js";
 
 const router = express.Router();
 
-// Changed .fields to .array to ensure req.files is a simple array
+// ✅ FINAL FIX ROUTE (IMPORTANT)
 router.post(
   "/",
   protect,
-  upload.array("images", 5), 
+  upload.fields([{ name: "images", maxCount: 5 }]),
   createReview
 );
 
+// GET REVIEWS
 router.get("/:productId", getProductReviews);
 
 export default router;
